122a196c0c310621a481e0bc64cf3ba30619aa8f
ismartdevteam <ulziiburend@gmail.com>
2014-09-10 08:47:07 -0700

    new quyer

--- server/functions/Query_data_func.php
+++ server/functions/Query_data_func.php
@@ -23,11 +23,12 @@ class Query {
 
     public static function getCar() {
         $db = DataBase::getInstance();
-        $query = "select s.id as seller_id, s.name as seller_name, c.id as carId,mark.name as markName ,cat.name as catName,m.name as model,body.name as bName,
-     c.modification,c.status,c.transmission,c.distance,dr.name as drivetrain,c.engine,c.price,c.fuel,c.viewcount,c.created_date,
-            i.id as image_id,i.image from car c left join body on c.body_id=body.id left join category_car
-             cat on c.category_car_id=cat.id left join model m on c.model_id=m.id left join mark on mark.id=m.mark_id left join image_car i
-           on c.id=i.car_id left join seller as s on c.seller_id=s.id left join drivetrain as dr on dr.id =c.drivetrain_id order by c.created_date,s.id, c.id,i.id ;  ";
+        $query = "select s.id as seller_id, s.name as seller_name, c.id as id,mark.id mark_id,mark.name as mark_name ,cat.id category_id,cat.name as category_name,m.id model_id,m.name as model,body.id body_id,body.name as body_name,
+     c.modification,c.status,c.transmission,c.distance,dr.name as drivetrain,c.engine,c.price,c.fuel,c.viewcount, c.created_date,
+            c.image_url ,c.order from car c left join body on c.body_id=body.id
+ left join category_car  cat on c.category_car_id=cat.id left join model m on c.model_id=m.id 
+left join mark on mark.id=m.mark_id  left join seller as s on c.seller_id=s.id
+ left join drivetrain as dr on dr.id =c.drivetrain_id order by c.order,cat.id,c.created_date desc; ";
         $check = $db->query($query);
         if ($check) {
             if ($check->num_rows > 0) {
@@ -39,35 +40,35 @@ class Query {
                 $imageIndex = -1;
                 $subrow = array();
                 while ($rows = mysqli_fetch_assoc($check)) {
-
-                    if ($carId != $rows['carId']) {
-                        $carIndex++;
-                        $imageIndex = -1;
-                        $carId = $rows['carId'];
-                        $subrow[$carIndex]['car_id'] = $carId;
-                        $subrow[$carIndex]['seller_id'] = $rows['seller_id'];
-                        $subrow[$carIndex]['seller_name'] = $rows['seller_name'];
-                        $subrow[$carIndex]['mark'] = $rows['markName'];
-                        $subrow[$carIndex]['cat'] = $rows['catName'];
-                        $subrow[$carIndex]['model'] = $rows['model'];
-                        $subrow[$carIndex]['body'] = $rows['bName'];
-                        $subrow[$carIndex]['modification'] = $rows['modification'];
-                        $subrow[$carIndex]['status'] = (int) $rows['status'];
-                        $subrow[$carIndex]['transmission'] = (int) $rows['transmission'];
-                        $subrow[$carIndex]['distance'] = (int) $rows['distance'];
-                        $subrow[$carIndex]['drivetrain'] = (int) $rows['drivetrain'];
-                        $subrow[$carIndex]['engine'] = (int) $rows['engine'];
-                        $subrow[$carIndex]['price'] = (int) $rows['price'];
-                        $subrow[$carIndex]['fuel'] = (int) $rows['fuel'];
-                        $subrow[$carIndex]['created_date'] = $rows['created_date'];
-                        $subrow[$carIndex]['count'] = (int) $rows['viewcount'];
-                        $subrow[$carIndex]['image'] = array();
-                    }
-                    if ($imageId != $rows['image_id']) {
-                        $imageIndex++;
-                        $imageId = $rows['image_id'];
-                        $subrow[$carIndex]['image'][$imageIndex]['image'] = $rows['image'];
-                    }
+                    $subrow[]=$rows;
+//                    if ($carId != $rows['carId']) {
+//                        $carIndex++;
+//                        $imageIndex = -1;
+//                        $carId = $rows['carId'];
+//                        $subrow[$carIndex]['car_id'] = $carId;
+//                        $subrow[$carIndex]['seller_id'] = $rows['seller_id'];
+//                        $subrow[$carIndex]['seller_name'] = $rows['seller_name'];
+//                        $subrow[$carIndex]['mark'] = $rows['markName'];
+//                        $subrow[$carIndex]['cat'] = $rows['catName'];
+//                        $subrow[$carIndex]['model'] = $rows['model'];
+//                        $subrow[$carIndex]['body'] = $rows['bName'];
+//                        $subrow[$carIndex]['modification'] = $rows['modification'];
+//                        $subrow[$carIndex]['status'] = (int) $rows['status'];
+//                        $subrow[$carIndex]['transmission'] = (int) $rows['transmission'];
+//                        $subrow[$carIndex]['distance'] = (int) $rows['distance'];
+//                        $subrow[$carIndex]['drivetrain'] = (int) $rows['drivetrain'];
+//                        $subrow[$carIndex]['engine'] = (int) $rows['engine'];
+//                        $subrow[$carIndex]['price'] = (int) $rows['price'];
+//                        $subrow[$carIndex]['fuel'] = (int) $rows['fuel'];
+//                        $subrow[$carIndex]['created_date'] = $rows['created_date'];
+//                        $subrow[$carIndex]['count'] = (int) $rows['viewcount'];
+//                        $subrow[$carIndex]['image'] = array();
+//                    }
+//                    if ($imageId != $rows['image_id']) {
+//                        $imageIndex++;
+//                        $imageId = $rows['image_id'];
+//                        $subrow[$carIndex]['image'][$imageIndex]['image'] = $rows['image'];
+//                    }
                 }
                 $result['data'] = $subrow;
             } else {
@@ -81,7 +82,8 @@ class Query {
 
     public static function getCompany() {
         $db = DataBase::getInstance();
-        $query = "select c.* ,ct.id type_id,ct.name type_name,img.id img_id,img.image_url  from company c left join image_company  img on c.id=img.company_id  join company_type ct on c.type_id=ct.id  order by c.order,ct.id asc;";
+        $query = "select c.* ,ct.id type_id,ct.name type_name  from company c "
+                . "left  join company_type ct on c.type_id=ct.id  order by c.order,ct.id asc;";
         $check = $db->query($query);
         if ($check) {
             if ($check->num_rows > 0) {
@@ -93,28 +95,8 @@ class Query {
                 $subrow = array();
                 while ($rows = mysqli_fetch_assoc($check)) {
 
-                    if ($comId != $rows['id']) {
-                        $carIndex++;
-                        $imageIndex = -1;
-                        $comId = $rows['id'];
-                        $subrow[$carIndex]['id'] = $comId;
-                        $subrow[$carIndex]['type_id'] = $rows['type_id'];
-                        $subrow[$carIndex]['type_name'] = $rows['type_name'];
-                        $subrow[$carIndex]['name'] = $rows['name'];
-                        $subrow[$carIndex]['description'] = $rows['description'];
-                        $subrow[$carIndex]['contact'] = $rows['contact'];
-                        $subrow[$carIndex]['address'] = $rows['address'];
-                        $subrow[$carIndex]['video_url'] = $rows['video_url'];
-                        $subrow[$carIndex]['location'] = $rows['location'];
-                        $subrow[$carIndex]['phone'] = $rows['phone'];
-                        $subrow[$carIndex]['order'] = $rows['order'];
-                        $subrow[$carIndex]['image'] = array();
-                    }
-                    if ($imageId != $rows['img_id']) {
-                        $imageIndex++;
-                        $imageId = $rows['img_id'];
-                        $subrow[$carIndex]['image'][$imageIndex]['image_url'] = $rows['image_url'];
-                    }
+                    $subrow[]=$rows;
+                   
                 }
                 $result['data'] = $subrow;
             } else {
